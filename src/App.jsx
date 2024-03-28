ddb2bfafc08cce96d06dffee6f97bb9b37c2b933
kgbinh <binhkhongg@gmail.com>
2024-03-28 14:27:26 +0700

    style button, input

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import { styled } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
-const Button = styled.button`
-  background-color: var(--color-brand-600);
-  font-size: 2rem;
-  color: white;
-  cursor: pointer;
-  border: none;
-  border-radius: 5px;
-  margin: 3rem;
-  padding: 1rem;
-  &:hover {
-    background-color: var(--color-brand-700);
-  }
-`;
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <Button>Click</Button>
+      <Button variation="danger">Click</Button>
+      <Input />
       Hello world
     </div>
   );
