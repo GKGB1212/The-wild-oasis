2b6336b757a01e665050683b2237b3abc40ba5b4
kgbinh <binhkhongg@gmail.com>
2024-03-28 14:08:44 +0700

    setup styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { styled } from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
+
+const Button = styled.button`
+  background-color: var(--color-brand-600);
+  font-size: 2rem;
+  color: white;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  margin: 3rem;
+  padding: 1rem;
+  &:hover {
+    background-color: var(--color-brand-700);
+  }
+`;
 function App() {
   return (
     <div>
+      <GlobalStyle />
+      <Button>Click</Button>
       Hello world
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
