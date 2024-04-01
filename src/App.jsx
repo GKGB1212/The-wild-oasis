245bb951c1dd4bfa433faefce312d560af9255e8
kgbinh <binhkhongg@gmail.com>
2024-04-01 15:51:34 +0700

    set up react Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyle from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -9,9 +11,17 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
@@ -28,7 +38,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/services/apiCabins.js
@@ -0,0 +1,10 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  let { data: cabins, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.log(error);
+    throw new Error("cannot get cabins");
+  }
+  return cabins;
+}
